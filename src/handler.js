b7e169a39dbdb881c1ab09d5225deee58e145eaa
Muhammad Bil Hakki <hakkibil543@gmail.com>
2023-06-01 20:10:35 +0700

    Tambah getByid

--- src/handler.js
+++ src/handler.js
@@ -26,5 +26,22 @@ const getData = (request, h) => {
     });
   };
 
+  //Mengambil Data berdasarkan id
+  const getDataById = (request, h) => {
+    const package = request.params.id; // Assuming the ID is passed as a route parameter
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM tourism WHERE id = ?'; // Update the query to include a WHERE clause
+      connection.query(query, [package], (error, results, fields) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(h.response(results));
+        }
+      });
+    }).catch(error => {
+      return h.response(error.message).code(500);
+    });
+  };
+
   
-  module.exports = { getData };
+  module.exports = { getData, getDataById };
