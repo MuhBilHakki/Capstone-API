7cda1091eaae6292070b3b844e35e5cc65a84ac7
Muhammad Bil Hakki <hakkibil543@gmail.com>
2023-06-09 21:22:07 +0700

    endpoint /location ke ML

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const Hapi = require('@hapi/hapi');
 const mysql = require('mysql');
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
@@ -58,7 +59,55 @@ const init = async () => {
             return h.response(error.message).code(500);
           });
         }
+      },
+      {
+        method: 'POST',
+        path: '/location',
+        handler: async (request, h) => {
+          try {
+            const { user_id, lat, lon } = request.payload;
+
+            // Mengirim data lat dan lon ke URL deploy model machine learning menggunakan axios
+            const response = await axios.post('https://sireko-ml-kowxf7ahmq-et.a.run.app', {
+              lat,
+              lon,
+              user_id
+            });
+
+            // Mendapatkan hasil prediksi dari respons
+            const prediction = response.data.prediction;
+
+            // Mengembalikan respons dengan hasil prediksi
+            return { user_id, lat, lon, prediction };
+          } catch (error) {
+            console.error(error);
+            return h.response({ message: 'Internal Server Error' }).code(500);
+          }
+        }
       }
+      // {
+      //   method: 'POST',
+      //   path: '/location',
+      //   handler: async (request, h) => {
+      //     try {
+      //       const { lat, lon } = request.payload; // Menggunakan request.payload untuk mendapatkan data lat dan lon
+      
+      //       // Contoh respons berhasil
+      //       return { lat, lon, message: 'Location received successfully' };
+      //     } catch (error) {
+      //       // Tangani kesalahan jika ada
+      //       console.error(error);
+      //       return h.response({ message: 'Internal Server Error' }).code(500);
+      //     }
+      //   }
+      // }
+      // {
+      //   method : 'GET',
+      //   path : '/locations',
+      //   handler : async(request,h) =>{
+
+      //   }
+      // }
       
   ]);
 
