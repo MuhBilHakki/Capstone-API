5ad9fce7f140d80ede2e6595a559fecfa233e0ed
Muhammad Bil Hakki <hakkibil543@gmail.com>
2023-06-13 19:44:17 +0700

    Tambah path location by userid, submit user profile, location lat & lon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Hapi = require('@hapi/hapi');
 const mysql = require('mysql');
 const axios = require('axios');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';
 
 const init = async () => {
@@ -16,7 +16,7 @@ const init = async () => {
   // Konfigurasi Connect Cloud SQL
   const connection = mysql.createConnection({
     host: '34.101.37.160',
-    user: 'root',
+    user: 'user-capstone',
     password: '123456',
     database: 'capstone'
   });
@@ -41,73 +41,92 @@ const init = async () => {
       }
       },
       {
-        method: 'GET',
-        path: '/destination/{id}',
-        handler: async (request, h) => {
-          const packageId = request.params.id;
-      
-          return new Promise((resolve, reject) => {
-            const query = 'SELECT * FROM tourism WHERE id = ?';
-            connection.query(query, [packageId], (error, results, fields) => {
-              if (error) {
-                reject(error);
-              } else {
-                resolve(h.response(results));
-              }
-            });
-          }).catch(error => {
-            return h.response(error.message).code(500);
-          });
-        }
-      },
-      {
         method: 'POST',
-        path: '/location',
+        path: '/location/byUser_Id',
         handler: async (request, h) => {
           try {
-            const { user_id, lat, lon } = request.payload;
+            const { user_id } = request.payload;
 
             // Mengirim data lat dan lon ke URL deploy model machine learning menggunakan axios
-            const response = await axios.post('https://sireko-ml-kowxf7ahmq-et.a.run.app', {
-              lat,
-              lon,
+            const response = await axios.post('https://sireko-ml-kowxf7ahmq-et.a.run.app/predict', {
               user_id
             });
 
             // Mendapatkan hasil prediksi dari respons
-            const prediction = response.data.prediction;
+            const prediction = response.data.recommendations;
+            
+            // Mengembalikan respons dengan hasil prediksi
+            return { prediction };
+            
+          } catch (error) {
+            console.error(error);
+            return h.response({ message: 'Internal Server Error' }).code(500);
+          }
+        }
+      },
+      {
+        method: 'POST',
+        path: '/submit_User_Profile',
+        handler: async (request, h) => {
+          try {
+            const { user_id, place_id, place_ratings } = request.payload;
+      
+            await new Promise((resolve, reject) => {
+              const query = 'INSERT INTO user_profile (user_id, place_id, place_ratings) VALUES (?, ?, ?)';
+              connection.query(query, [user_id, place_id, place_ratings], (error, results, fields) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(results);
+                }
+              });
+            });
+      
+            return h.response({ message: 'User profile submitted successfully' });
+          } catch (error) {
+            console.error(error);
+            return h.response({ message: 'Internal Server Error' }).code(500);
+          }
+        }
+      },      
+      {
+        method: 'POST',
+        path: '/location/byLat_Lon',
+        handler: async (request, h) => {
+          try {
+            const { latitude, longitude } = request.payload;
+
+            // Mengirim data lat dan lon ke URL deploy model machine learning menggunakan axios
+            const response = await axios.post('https://sireko-ml-kowxf7ahmq-et.a.run.app/predict_loc', {
+              latitude,
+              longitude
+            });
+
+            // Mendapatkan hasil prediksi dari respons
+            // const predictions = response.data.map(location => {
+            //     return {
+            //         id: location.Place_Id,
+            //         name: location.Place_Name,
+            //         description : location.Description,
+            //         rating : location.Rating,
+            //         city : location.City,
+            //         latitude: location.Lat,
+            //         longitude: location.Long,
+            //         image : location.Image
+            //     };
+            // });
+            const predictions = response.data;
+              
 
             // Mengembalikan respons dengan hasil prediksi
-            return { user_id, lat, lon, prediction };
+            return { predictions };
+            
           } catch (error) {
             console.error(error);
             return h.response({ message: 'Internal Server Error' }).code(500);
           }
         }
       }
-      // {
-      //   method: 'POST',
-      //   path: '/location',
-      //   handler: async (request, h) => {
-      //     try {
-      //       const { lat, lon } = request.payload; // Menggunakan request.payload untuk mendapatkan data lat dan lon
-      
-      //       // Contoh respons berhasil
-      //       return { lat, lon, message: 'Location received successfully' };
-      //     } catch (error) {
-      //       // Tangani kesalahan jika ada
-      //       console.error(error);
-      //       return h.response({ message: 'Internal Server Error' }).code(500);
-      //     }
-      //   }
-      // }
-      // {
-      //   method : 'GET',
-      //   path : '/locations',
-      //   handler : async(request,h) =>{
-
-      //   }
-      // }
       
   ]);
 
